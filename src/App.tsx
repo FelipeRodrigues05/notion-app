896e15946ec5618188cb1ff35fe36d0479353987
FelipeBarbosaRodrigues <felipe@praja.net>
2023-07-17 12:28:01 -0300

    Added Responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import WebEditor from "./components/WebEditor";
 export default function App() {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-l from-slate-900 via-purple-900 to-slate-900 text-zinc-600 dark:text-zinc-900">
+      <div className="min-h-screen bg-gradient-to-l from-slate-900 via-purple-900 to-slate-900 text-zinc-600 sm:pt-0 lg:pt-8 dark:text-zinc-900">
         <div className="max-w-[75rem] max-h-[50rem] mx-auto rounded-xl shadow-sm border-black/20 overflow-hidden grid grid-cols-[8rem_1fr] md:grid-cols-[16rem_1fr] sm:grid-cols-[8rem_1fr] bg-zinc-300 dark:bg-zinc-800">
           <aside className="border-r p-4 bg-zinc-400 border-zinc-400 dark:border-zinc-900 dark:bg-zinc-950">
             <div className="flex gap-2 group flex-row">
